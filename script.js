7161e98583465fd4dc45d0736263fdf632f79da3
bhatiamanan <mananbhatia75@gmail.com>
2024-06-22 16:38:39 +0530

    feat: Add form submission functionality to contact form

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-document.querySelector('.cross').style.display =  'none';
-document.querySelector('.hamburger').addEventListener("click", ()=>{
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", () => {
     document.querySelector('.sidebar').classList.toggle('sidebarGo');
-    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
-        document.querySelector('.cross').style.display ='none';
-        document.querySelector('.ham').style.display ='inline';
-    }else{
-        document.querySelector('.ham').style.display ='none';
-        setTimeout(()=> {
-            document.querySelector('.cross').style.display ='inline'
+    if (document.querySelector('.sidebar').classList.contains('sidebarGo')) {
+        document.querySelector('.cross').style.display = 'none';
+        document.querySelector('.ham').style.display = 'inline';
+    } else {
+        document.querySelector('.ham').style.display = 'none';
+        setTimeout(() => {
+            document.querySelector('.cross').style.display = 'inline'
         }, 280);
     }
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var form = event.target;
+    var formData = new FormData(form);
+    var status = document.getElementById('form-status');
+
+    fetch('https://formspree.io/f/xldrrjwg', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            status.innerHTML = "Thanks for your submission!";
+            form.reset();
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+                } else {
+                    status.innerHTML = "Oops! There was a problem submitting your form.";
+                }
+            });
+        }
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form.";
+    });
 });
